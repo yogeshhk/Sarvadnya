504a8629309c4e28d5674acde56d5b8310e97097
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2023-04-26 17:16:35 +0530

    Added smaller xls

--- src/bAbi_tasks/bAbi_cognition_meter.py
+++ src/bAbi_tasks/bAbi_cognition_meter.py
@@ -5,31 +5,41 @@
 
 # Assuming OPENAI_API_KEY set in Environment variables
 
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI, HuggingFaceHub
 from langchain import PromptTemplate
 from langchain import LLMChain
 import pandas as pd
-bool_score = False
+
+bool_score = True
 total_score = 0
 count = 0
 
 template = "{context} {prompt}"
 prompt = PromptTemplate(template=template, input_variables=['context', 'prompt'])
 
-llm = OpenAI(temperature=0)
-chain = LLMChain(llm=llm, prompt=prompt)
+llms = [{'name': 'OpenAI', 'model': OpenAI(temperature=0)}]#,
+       # {'name': 'Flan', 'model':  HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature": 1e-10})}]
 
-df = pd.read_excel(r'data/Basic20.xlsx')
-for index, row in df.iterrows():
-    context = (row['Context']).replace("\n", " ")
-    prompts = (row['Prompts']).split("\n")
-    labels = (row['Labels']).split("\n")
-    for prompt, label in zip(prompts, labels):
-        print(f"Context: {context}\nPrompt:{prompt}\nLabel: {label}")
-        keywords = {'context': context, 'prompt': prompt}
-        print(f"Response: {chain.run(keywords).strip()}")
-        str_score = input('Score? 0 for Wrong, 1 for Perfect : \n')
-        total_score += float(str_score)
-        count += 1
+df = pd.read_excel(r'data/Test2.xlsx')
 
-print(f"LLM score : {total_score/count}")
+for llm_dict in llms:
+    llm_name = llm_dict['name']
+    llm_model = llm_dict['model']
+    chain = LLMChain(llm=llm_model, prompt=prompt)
+
+    df.reset_index()
+    for index, row in df.iterrows():
+        context = (row['Context']).replace("\n", " ")
+        prompts = (row['Prompts']).split("\n")
+        labels = (row['Labels']).split("\n")
+        for prompt, label in zip(prompts, labels):
+            print(f"Context: {context}\nPrompt:{prompt}\nLabel: {label}")
+            keywords = {'context': context, 'prompt': prompt}
+            print(f"Response: {chain.run(keywords).strip()}")
+            if bool_score:
+                str_score = input('Score? 0 for Wrong, 1 for Perfect : ')
+                total_score += float(str_score)
+                count += 1
+
+    if count:
+        print(f"LLM score for {llm_name}: {total_score / count}")
