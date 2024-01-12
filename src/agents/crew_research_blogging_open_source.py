00635a381ace647be66490d0993e30f18159a2cb
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2024-01-12 13:43:31 +0530

    Added crew example

--- src/agents/crew_research_blogging_open_source.py
+++ src/agents/crew_research_blogging_open_source.py
@@ -13,91 +13,69 @@ from langchain_openai.llms import OpenAI
 
 # Configure OpenAI settings
 # os.environ["OPENAI_API_KEY"] = "YOUR KEY"
-
 lmstudio_llm = OpenAI(temperature=0, openai_api_base="http://localhost:1234/v1")
 
 search_tool = DuckDuckGoSearchRun()
 
 # Define your agents with roles and goals
-explorer = Agent(
-    role='Senior Researcher',
-    goal="""Find and explore the most exciting projects and companies in 
-  AI and machine learning in 2024""",
-    backstory="""You are and Expert strategist that knows how to spot
-              emerging trends and companies in AI, tech and machine learning. 
-              You're great at finding interesting, exciting projects in Open 
-              Source/Indie Hacker space. You look for exciting, rising
-              new github ai projects that get a lot of attention.
+coach = Agent(
+    role='Senior Career Coach',
+    goal="""Find and explore the most exciting career skills related to tech and AI in 2024""",
+    backstory="""You are and Expert Career Coach that knows how to spot
+              emerging trends and skills needed in AI and tech. 
               """,
     verbose=True,
     allow_delegation=False,
     tools=[search_tool],
     llm=lmstudio_llm
 )
-writer = Agent(
-    role='Senior Technical Writer',
-    goal="""Write engaging and interesting blog post about latest AI projects 
-            using simple, layman vocabulary""",
-    backstory="""You are an Expert Writer on technical innovation, especially 
-              in the field of AI and machine learning. You know how to write in 
-              engaging, interesting but simple, straightforward and concise. You know 
-              how to present complicated technical terms to general audience in a 
-              fun way by using layman words.""",
+influencer = Agent(
+    role='LinkedIn Influencer Writer',
+    goal="""Write engaging and interesting LinkedIn post in maximum 200 words. Add relevant emojis""",
+    backstory="""You are an Expert Writer on LinkedIn in the field of AI and tech""",
     verbose=True,
     allow_delegation=True,
     llm=lmstudio_llm
 )
 critic = Agent(
     role='Expert Writing Critic',
-    goal="""Provide feedback and criticize blog post drafts. Make sure that the 
-            tone and writing style is compelling, simple and concise""",
-    backstory="""You are an Expert at providing feedback to the technical
-              writers. You can tell when a blog text isn't concise,
-              simple or engaging enough. You know how to provide helpful feedback that 
-              can improve any text. You know how to make sure that text 
-              stays technical and insightful by using layman terms. """,
+    goal="""Provide feedback and criticize post drafts. """,
+    backstory="""You are an Expert at providing feedback to the technical writers. 
+    Make sure that the the suggestions are actionable, compelling, simple and concise.
+    Also make sure that the post is within 200 words, has emojis and relevant hashtags.""",
     verbose=True,
     allow_delegation=True,
     llm=lmstudio_llm
 )
 
 # Create tasks for your agents
-task_report = Task(
-    description="""Make a detailed report on the latest rising projects 
-                  in AI and machine learning space. Find emerging trends, topics,
-                  rising github projects and producthunt apps that have people talking. 
-                  Spot the most recent trends in the last 2-3 weeks.
-                  Your final answer MUST be a full analysis report, with bullet points 
-                  and with at least 5 exciting new AI projects and tools
-                  """,
-    agent=explorer
+task_search = Task(
+    description="""Make a detailed report on the latest rising skills 
+                  in AI and tech space. Your final answer MUST be a list of at least 5 exciting new AI and tech skills
+                in the format of bullet points.""",
+    agent=coach
 )
 
-task_blog = Task(
-    description="""Write a blog article with a short but impactful headline 
-                  and at least 10 paragraphs. Blog should summarize the latest 
-                  AI and machine learning accomplishments and rising github projects and 
-                  companies in the AI space. Style and tone should be compelling and concise,
-                  fun, technical but also use layman words for the general public. Name 
-                  specific new, exciting projects, apps and companies in AI world.
+task_post = Task(
+    description="""Write a LinkedIn post with a short but impactful headline 
+                  and at max 200 words. It should list the latest 
+                  AI and tech skills which are going to be in demand. 
                   """,
-    agent=writer
+    agent=influencer
 )
 
 task_critique = Task(
-    description="""Identify parts of the blog that aren't written concise enough
-                   and improve them. Make sure that the blog has engaging 
-                  headline with 30 characters max, and that there are at least 10 paragraphs. 
-                  Blog needs to be written in such a way that a 15-year old 
-                  can  understand it.
+    description="""Identify parts of the post that aren't written concise enough
+                   and improve them. Make sure that the post has engaging 
+                  headline with 30 characters max, and that there are at max 200 words. 
                   """,
     agent=critic
 )
 
 # Instantiate your crew with a sequential process
 crew = Crew(
-    agents=[explorer, writer, critic],
-    tasks=[task_report, task_blog, task_critique],
+    agents=[coach, influencer, critic],
+    tasks=[task_search, task_post, task_critique],
     verbose=2,
     process=Process.sequential  # Sequential process will have tasks executed one after the other
     # and the outcome of the previous one is passed as extra content into this next.
