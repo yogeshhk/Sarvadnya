83de17fdf34d3b45a9a251cf52139fda7019196f
firstgit <chaitrali131@gmail.com>
2025-07-02 00:21:38 +0530

    ask_faqs_chatbot

--- src/ask_faqs_chatbot/faqengine.py
+++ src/ask_faqs_chatbot/faqengine.py
@@ -1,10 +1,8 @@
 import os
-
 import nltk
 import pandas as pd
 import numpy as np
 from nltk.stem.lancaster import LancasterStemmer
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import LabelEncoder as LE
 from sklearn.svm import SVC
@@ -12,7 +10,6 @@ from sklearn.svm import SVC
 from vectorizers.factory import get_vectoriser
 
 import faiss
-
 
 class FaqEngine:
     def __init__(self, faqslist, type='tfidf'):
@@ -29,99 +26,65 @@ class FaqEngine:
         return ' '.join(stemmed_words)
 
     def build_model(self, type):
-
-        self.vectorizer = get_vectoriser(type)  # TfidfVectorizer(min_df=1, stop_words='english')
+        self.vectorizer = get_vectoriser(type)
         dataframeslist = [pd.read_csv(csvfile).dropna() for csvfile in self.faqslist]
         self.data = pd.concat(dataframeslist, ignore_index=True)
-        self.data['Clean_Question'] = self.data['Question'].apply(lambda x : self.cleanup(x))
-        self.data['Question_embeddings'] = list(self.vectorizer.vectorize(self.data['Clean_Question'].tolist()))
+        self.data['Clean_Question'] = self.data['Question'].apply(self.cleanup)
+
+        embeddings = self.vectorizer.vectorize(self.data['Clean_Question'].tolist())
+        embeddings = np.array(embeddings).astype('float32')  # FAISS requires float32
+        self.data['Question_embeddings'] = list(embeddings)
         self.questions = self.data['Question'].values
-        X = self.data['Question_embeddings'].tolist()
-        
-        X = np.array(X)
-        d = X.shape[1]
+
+        # FAISS index
+        d = embeddings.shape[1]
         index = faiss.IndexFlatL2(d)
-        if index.is_trained :
-            index.add(X)
+        if index.is_trained:
+            index.add(embeddings)
         self.vector_store = index
-        # Loop wise version for question embedding generation
-        # questions_cleaned = []
-        # for question in self.questions:
-        #     questions_cleaned.append(self.cleanup(question))
 
-        # X = self.vectorizer.vectorize(questions_cleaned)
-
-        # Under following cases, we dont do classification
-        # 'Class' column abscent
-        # 'Class' column has same values
-        if 'Class' not in list(self.data.columns):
+        # Classification setup
+        if 'Class' not in self.data.columns:
             return
 
         y = self.data['Class'].values.tolist()
-        if len(set(y)) < 2: # 0 or 1
+        if len(set(y)) < 2:
             return
 
         y = self.le.fit_transform(y)
-
-        trainx, testx, trainy, testy = tts(X, y, test_size=.25, random_state=42)
+        trainx, testx, trainy, testy = tts(embeddings, y, test_size=0.25, random_state=42)
 
         self.classifier = SVC(kernel='linear')
         self.classifier.fit(trainx, trainy)
-        # print("SVC:", self.model.score(testx, testy))        
 
     def query(self, usr):
-        # print("User typed : " + usr)
         try:
             cleaned_usr = self.cleanup(usr)
-            t_usr_array = self.vectorizer.query(cleaned_usr)
+            t_usr_array = self.vectorizer.query(cleaned_usr).astype('float32')  # Ensure float32
+
             if self.classifier:
                 prediction = self.classifier.predict(t_usr_array)[0]
                 class_ = self.le.inverse_transform([prediction])[0]
-                # print("Class " + class_)
                 questionset = self.data[self.data['Class'] == class_]
             else:
                 questionset = self.data
 
-            # threshold = 0.7
-            
-            # Vectorized implementation of cosine similarity usage for fast execution
-            #cos_sims = cosine_similarity(questionset['Question_embeddings'].tolist(), t_usr_array)
-
-            # Top most similar question
             top_k = 1
-            
-            #calling FAISS search
             D, I = self.vector_store.search(t_usr_array, top_k)
-            
             question_index = int(I[0][0])
             return self.data['Answer'][question_index]
-            
-            # Loop wise implementation of cosine similarity usage
-            # cos_sims = []
-            # for question in questionset['Question']:
-            #     cleaned_question = self.cleanup(question)
-            #     question_arr = self.vectorizer.query(cleaned_question)
-            #     sims = cosine_similarity(question_arr, t_usr_array)
-            #     # if sims > threshold:
-            #     cos_sims.append(sims)
 
-            # print("scores " + str(cos_sims))
-            # commenting this code as we use FAISS vector store. 
-            '''if len(cos_sims) > 0:
-                ind = np.argmax(cos_sims)
-                return self.data['Answer'][questionset.index[ind]]'''
-                # ind = cos_sims.index(max(cos_sims))
-                # print(ind)
-                # print(questionset.index[ind])
-                
         except Exception as e:
             print(e)
-            return "Could not follow your question [" + usr + "], Try again"
+            return f"Could not follow your question [{usr}], Try again."
 
 
 if __name__ == "__main__":
-    base_path = os.path.join(os.path.dirname(os.path.abspath( __file__ )),"data")
-    faqslist = [os.path.join(base_path,"Greetings.csv"), os.path.join(base_path,"GST FAQs 2.csv")]
-    faqmodel = FaqEngine(faqslist, 'tfidf')
+    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    faqslist = [
+        os.path.join(base_path, "Greetings.csv"),
+        os.path.join(base_path, "GST FAQs 2.csv")
+    ]
+    faqmodel = FaqEngine(faqslist, 'sentence-transformer')  # or 'groq' or your chosen tag
     response = faqmodel.query("Hi")
     print(response)
--- src/ask_faqs_chatbot/vectorizers/bertgenerator.py
+++ src/ask_faqs_chatbot/vectorizers/bertgenerator.py
@@ -1,27 +1,50 @@
-# Ref: https://github.com/hanxiao/bert-as-service#building-a-qa-semantic-search-engine-in-3-minutes
-# need to have bert server running in a separate window
-# bert-serving-start -num_worker=1 -model_dir=D:/Yogesh/Education/DataScience/Datasets/uncased_L-12_H-768_A-12 -max_seq_len NONE
+# # Ref: https://github.com/hanxiao/bert-as-service#building-a-qa-semantic-search-engine-in-3-minutes
+# # need to have bert server running in a separate window
+# # bert-serving-start -num_worker=1 -model_dir=D:/Yogesh/Education/DataScience/Datasets/uncased_L-12_H-768_A-12 -max_seq_len NONE
 
-from bert_serving.client import BertClient
+# from bert_serving.client import BertClient
+# import numpy as np
+
+# ## Todo : this is still working on pretrained model, not corpus specific
+# class BertGenerator:
+    
+#     def __init__(self, model_dir, size=300):
+#         self.pretrained_model_dir = model_dir + "uncased_L-12_H-768_A-12/"
+#         self.model_dir = model_dir
+#         self.bc = BertClient(port=5555, port_out=5556)
+
+#     def vectorize(self, clean_questions):
+#         return self.bc.encode(clean_questions)
+        
+#     def query(self, clean_usr_msg):
+#         t_usr_array= None
+#         try:
+#             t_usr_array = self.bc.encode([clean_usr_msg])[0]
+#         except Exception as e:
+#             print(e)
+#             return "Could not follow your question [" + t_usr_array + "], Try again"
+            
+#         return np.array([t_usr_array])
+from sentence_transformers import SentenceTransformer
 import numpy as np
 
-## Todo : this is still working on pretrained model, not corpus specific
 class BertGenerator:
-    
-    def __init__(self, model_dir, size=300):
-        self.pretrained_model_dir = model_dir + "uncased_L-12_H-768_A-12/"
-        self.model_dir = model_dir
-        self.bc = BertClient(port=5555, port_out=5556)
+    def __init__(self):
+        # Load a fast, compact model (you can change it later if needed)
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
     def vectorize(self, clean_questions):
-        return self.bc.encode(clean_questions)
-        
-    def query(self, clean_usr_msg):
-        t_usr_array= None
         try:
-            t_usr_array = self.bc.encode([clean_usr_msg])[0]
+            return self.model.encode(clean_questions)
         except Exception as e:
-            print(e)
-            return "Could not follow your question [" + t_usr_array + "], Try again"
-            
-        return np.array([t_usr_array])
+            print("SentenceTransformer vectorize error:", e)
+            return np.zeros((len(clean_questions), 384))
+
+    def query(self, clean_usr_msg):
+        try:
+            emb = self.model.encode([clean_usr_msg])[0]
+            return np.array([emb])
+        except Exception as e:
+            print("SentenceTransformer query error:", e)
+            return np.zeros((1, 384))
+#sentence transformer is used since bertgenerator is depricated
